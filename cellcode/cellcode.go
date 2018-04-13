3a49efc16668b922745d5c430229c181df5d50e7
hackurity <hackurity01@gmail.com>
2018-04-13 20:42:45 +0900

    Update docker && cellcode

--- cellcode/cellcode.go
+++ cellcode/cellcode.go
@@ -1,5 +1,40 @@
-package cellcode
+package main
+
+import (
+	"os"
+	"plugin"
+)
+
+type ICode interface {
+	Query()
+	Invoke()
+}
 
 func main() {
+	if len(os.Args) != 2 {
+		os.Exit(1)
+	}
 
+	iCodePath := os.Args[1]
+
+	plug, err := plugin.Open(iCodePath)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	iCodePlugin, err := plug.Lookup("ICodeInstance")
+	if err != nil {
+		os.Exit(1)
+	}
+
+	iCode := iCodePlugin.(ICode)
+
+	// ToDo : Socket Connection
+	// ToDo : Setting Cell
+
+	if(true) {
+		iCode.Query()
+	} else {
+		iCode.Invoke()
+	}
 }
