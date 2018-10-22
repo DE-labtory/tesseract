7baf45d323ff6292e21192d117a9ee2be88d432c
ParkHaeSung <hea9549@gmail.com>
2018-10-22 16:30:16 +0900

    add mock icode

---
+++ network.go
@@ -0,0 +1,5 @@
+package docker
+
+type Network struct {
+	Name string
+}
---
+++ volume.go
@@ -0,0 +1,20 @@
+package docker
+
+// referenced go-docker repository: https://github.com/docker/go-docker/blob/master/api/types/volume.go
+type Volume struct {
+	CreatedAt  string
+	Driver     string
+	Mountpoint string
+	Name       string
+	Options    map[string]string
+}
+
+func NewVolume(createdAt, driver, mountpoint, name string, opts map[string]string) Volume {
+	return Volume{
+		CreatedAt:  createdAt,
+		Driver:     driver,
+		Mountpoint: mountpoint,
+		Name:       name,
+		Options:    opts,
+	}
+}
